0cf718e6fe9654bd0ff1d5d91e694642c67dd5c1
Kevin Ludlam <kevinlud92@gmail.com>
2020-03-11 16:58:52 -0700

    sherel done by .75

--- client/src/Dialogue/Dialogue.js
+++ client/src/Dialogue/Dialogue.js
@@ -287,19 +287,19 @@ const Dialogue = {
     27:
     {
       text: "Thank you for asking. We are confident we will have a director in place and a published paper in the next year.",
-      nextDialogue: [31],
+      nextDialogue: 31,
       addScore: 3,
     },
     28:
     {
       text: "As we all know, climate change research is crucial, but we need to connect the studies to the work in our hatchery and toxicology lab.",
-      nextDialogue: [32],
+      nextDialogue: 32,
       addScore: 5,
     },
     29:
     {
       text: "I’m pleased to hear that. Let’s review our pledge form",
-      nextDialogue: [33],
+      nextDialogue: 33,
       addScore: 0,
     },
     30:
@@ -341,7 +341,7 @@ const Dialogue = {
     36:
     {
       text: "Thank you. Your vision is vital to the success of this project. The total cost will be $50,000. Would you consider making a leadership gift of $25,000?",
-      nextDialogue: [39],
+      nextDialogue: 39,
       addScore: 5,
     },
     37:
@@ -353,13 +353,13 @@ const Dialogue = {
     38:
     {
       text: "We’d be happy to do that. I’ll write the first check for $10, 000 and invite my friends to join me.",
-      nextDialogue: [40],
+      nextDialogue: 40,
       addScore: 0,
     },
     39:
     {
       text: "I’d be thrilled to do that. I would also like to help by inviting some friends who support climate change research to join me.",
-      nextDialogue: [40],
+      nextDialogue: 40,
       addScore: 0,
     },
     40:
@@ -374,7 +374,392 @@ const Dialogue = {
   },
   Romero2: {
 
-  }
+  },
+  Sherl1: {
+    1:
+    {
+      text: "Hello, this is Sharrel",
+      nextDialogue: [2,3,4],
+      addScore: 0,
+    },   
+    2:
+    {
+      text: "Hello Sharrel, this is (PlayerName) from the Aquarium, I was wondering if you had a minute to talk?",
+      nextDialogue: 5,
+      addScore: 1,
+    },
+    3:
+    {
+      text: "Hello Sharrel, this is (PlayerName) from the Aquarium, I was wondering if you had a minute to talk?",
+      nextDialogue: 6,
+      addScore: 3,
+    },
+    4:
+    {
+      text: "Hi Sharrel! It's (PlayerName) from the Aquarium.Just wondering if we can pick up our conversation from the other day.",
+      nextDialogue: 6,
+      addScore: 5,
+    },
+    5:
+    {
+      text: "I'm sorry, I'm very busy right now, but I have a minute or two to talk.Will this be quick ?",
+      nextDialogue: [7,8],
+      addScore: 0,
+    },
+    6:
+    {
+      text: "Sure I have time to talk. What do we need to discuss? How's the Aquarium doing?",
+      nextDialogue: [9,10],
+      addScore: 0,
+    },
+    7:
+    {
+      text: "Yes, this'll be just a quick chat.Is that ok ?",
+      nextDialogue: "Gate1",
+      addScore: 0,
+    },
+    8:
+    {
+      text: "Oh, I'm sorry for bothering you; I can make this brief. We are just about to reopen the touch tank exhibit.",
+      nextDialogue: "Gate1",
+      addScore: 2,
+    },
+    9:
+    {
+      text: "Things are good. I just wanted to let you know we secured some foundation funding to support marine illustrations in our new exhibits.",
+      nextDialogue: "Gate1",
+      addScore: 4,
+    },
+    10:
+    {
+      text: "Things are going well. I know how passionate you are about marine illustrations. We have just secured an exciting new artist for a 6 month residency to work on our illustrations in our galleries.",
+      nextDialogue: "Gate1",
+      addScore: 5,
+    },
+    "Gate1":
+    {
+      text: "",
+      nextDialogue: 0,
+      addScore: ["Gate1-1", "Gate1-2", "Gate1-3"],
+    },
+    "Gate1-1":
+    {
+      text: "I don't really have much time right now",
+      nextDialogue: [14, 15],
+      addScore: 0,
+    },
+    "Gate1-2":
+    {
+      text: "Sounds like your guys are on the right track",
+      nextDialogue: [15,16],
+      addScore: 0,
+    },
+    "Gate1-3":
+    {
+      text: "That's great to hear! Original artwork is such a powerful tool for engagement.",
+      nextDialogue: [16,17],
+      addScore: 0,
+    },
+    14:
+    {
+      text: "You know, why don't you just come over sometime instead and I can catch you up on what's been going on?",
+      nextDialogue: 18,
+      addScore: 0,
+    },
+    15:
+    {
+      text: "I could tell you more when you have time.",
+      nextDialogue: 19,
+      addScore: 2,
+    },
+    16:
+    {
+      text: "Thank you Sharrel! I was hoping you'd be interested in hearing about our plans at the Aquarium.",
+      nextDialogue: 20,
+      addScore: 3,
+    },
+    17:
+    {
+      text: "If you'd like, we could schedule a time to discuss the Aquarium's progress and future.",
+      nextDialogue: 21,
+      addScore: 4,
+    },
+    18:
+    {
+      text: "I really don't have time for this.",
+      nextDialogue: ["End"],
+      addScore: 0,
+    },
+    19:
+    {
+      text: "We have so many commitments right now, I just don't think this is a good time.",
+      nextDialogue: [22,23],
+      addScore: 0,
+    },
+    20:
+    {
+      text: "Ok, I'd like to know more.",
+      nextDialogue: [23,24],
+      addScore: 0,
+    },
+    21:
+    {
+      text: "I would like that.",
+      nextDialogue: [24,25],
+      addScore: 0,
+    },
+    22:
+    {
+      text: "Trust me, you'll want to hear this.",
+      nextDialogue: "Gate2",
+      addScore: 0,
+    },
+    23:
+    {
+      text: "I think this would come across better if we met in person.",
+      nextDialogue: "Gate2",
+      addScore: 2,
+    },
+    24:
+    {
+      text: "I would love to set up a time where I could fill you in and discuss how you can get involved.",
+      nextDialogue: "Gate2",
+      addScore: 3,
+    },
+    25:
+    {
+      text: "I have some ideas about how you can get involved that I would like to discuss with you in person.How does your schedule look for the next few weeks ?",
+      nextDialogue: "Gate2",
+      addScore: 4,
+    },
+    "Gate2":
+    {
+      text: "",
+      nextDialogue: ["Gate2-1","Gate2-2", "Gate2-3","Gate2-4"],
+      addScore: 0,
+    },
+    "Gate2-1":
+    {
+      text: "I will have to think about this.",
+      nextDialogue: ["End"],
+      addScore: 0,
+    },
+    "Gate2-2":
+    {
+      text: "Sure we can meet. Let's set up something for next month.",
+      nextDialogue: ["End"],
+      addScore: 0,
+    },
+    "Gate2-3":
+    {
+      text: "I'd like that, how about we meet this Friday ?",
+      nextDialogue: [30],
+      addScore: 0,
+    },
+    "Gate2-4":
+    {
+      text: "No, I don't think I have the capacity to add anything right now.",
+      nextDialogue: [30],
+      addScore: 0,
+    },
+    30:
+    {
+      text: "How about you come to my house and we can chat ?",
+      nextDialogue: [31,32,33],
+      addScore: 0,
+    },
+    31:
+    {
+      text: "Actually, I know the Maitre' D at that new restaurant Chez Jacques.I heard it's fantastic, have you been ?",
+      nextDialogue: 34,
+      addScore: 0,
+    },
+    32:
+    {
+      text: "I don't want to put you to any trouble. Why don't we meet at the café on 5th ?",
+      nextDialogue: 35,
+      addScore: 0,
+    },
+    33:
+    {
+      text: "Thank you so much, I'd love to see your home.",
+      nextDialogue: 36,
+      addScore: 0,
+    },
+    34:
+    {
+      text: "Oh I've heard wonderful things about it.The waitlist for reservations is so long.Let's go.",
+      nextDialogue: ["End"],
+      addScore: 0,
+    },
+    35:
+    {
+      text: "That's really thoughtful of you.I like that café.",
+      nextDialogue: ["End"],
+      addScore: 0,
+    },
+    36:
+    {
+      text: "Terrific, I'll send you my address.",
+      nextDialogue: ["End"],
+      addScore: 0,
+    },
+    
+  },
+
+  Sherl2: {
+    1:
+    {
+      text: "Thank you for taking the time to meet with me. I've been looking forward to sharing our plans to incorporate marine illustrations throughout the Aquarium.",
+      nextDialogue: 2,
+      addScore: 0,
+    },
+    2:
+    {
+      text: "I've been looking forward to hearing your plans.",
+      nextDialogue: [3,4,5,6],
+      addScore: 0,
+    },
+    3:
+    {
+      text: "I just wanted to let you know we secured some foundation funding to support marine illustrations in our new exhibits.I know we talked about this before, but I just think it is so wonderful and we are really excited about it.",
+      nextDialogue: 7,
+      addScore: 1,
+    },
+    4:
+    {
+      text: "I know how passionate you are about marine illustration. We have secured an exciting new artist for a 6 month residency to work on illustrations around the Aquarium. I'm sure you are as excited as we are",
+      nextDialogue: 8,
+      addScore: 2,
+    },
+    5:
+    {
+      text: "We appreciate your passion for contemporary art and marine illustrations and are hoping you would be interested in expanding the illustrations we've planned for the touch tank.",
+      nextDialogue: 9,
+      addScore: 3,
+    },
+    6:
+    {
+      text: "Your commitment to contemporary art and its power to engage people has inspired us to expand our marine illustration plans to the touch tank exhibit. It is the most visited room at the Aquarium and this will create a unique multisensory experience for all of our guests.",
+      nextDialogue: 9,
+      addScore: 4,
+    },
+    7:
+    {
+      text: "You had mentioned that on the phone.Is there anything else you wanted to discuss ?",
+      nextDialogue: [10,11],
+      addScore: 0,
+    },
+    8:
+    {
+      text: "I was pleased to hear that.Why don't you tell me more about those plans ?",
+      nextDialogue: [11,12],
+      addScore: 0,
+    },
+    9:
+    {
+      text: "Your team is really on the right track. Engagement is such a popular buzzword these days and so few organizations realize what it takes to do it correctly.",
+      nextDialogue: [12,13],
+      addScore: 0,
+    },
+    10:
+    {
+      text: "Oh yes actually, we have secured an exciting new artist for a 6 month residency.",
+      nextDialogue: "Gate1",
+      addScore: 1,
+    },
+    11:
+    {
+      text: "We are actually hoping to add illustrations to the touch tank",
+      nextDialogue: "Gate1",
+      addScore: 2,
+    },
+    12:
+    {
+      text: "Expanding illustrations to the touch tank room will create a unique multisensory experience for our guests in the most visited room in the Aquarium.",
+      nextDialogue: "Gate1",
+      addScore: 3,
+    },
+    13:
+    {
+      text: "I am so happy to hear you say that.We have had an interdisciplinary team working on this for the last two years.",
+      nextDialogue: "Gate1",
+      addScore: 4,
+    },
+    "Gate1":
+    {
+      text: "",
+      nextDialogue: 0,
+      addScore: 0,
+    },
+    "Gate1-1":
+    {
+      text: "No, I don’t think that I have the capacity to add anything right now.",
+      nextDialogue: ["End"],
+      addScore: 0,
+    },
+    "Gate1-2":
+    {
+      text: "I will have to think about this.",
+      nextDialogue: ["End"],
+      addScore: 0,
+    },
+    "Gate1-3":
+    {
+      text: "Thanks for walking me through everything.I’m impressed with your commitment to illustration as a form of engagement, and I’d like to help.",
+      nextDialogue: [18,19],
+      addScore: 0,
+    },
+    "Gate1-4":
+    {
+      text: "This is so exciting. It is wonderful that your team is so thoughtful about illustration and I would love to extend those plans to the touch tank.",
+      nextDialogue: [19,20],
+      addScore: 0,
+    },
+    18:
+    {
+      text: "",
+      nextDialogue: 0,
+      addScore: 0,
+    },
+    19:
+    {
+      text: "",
+      nextDialogue: 0,
+      addScore: 0,
+    },
+    20:
+    {
+      text: "",
+      nextDialogue: 0,
+      addScore: 0,
+    },
+    21:
+    {
+      text: "",
+      nextDialogue: 0,
+      addScore: 0,
+    },
+    22:
+    {
+      text: "",
+      nextDialogue: 0,
+      addScore: 0,
+    },
+    23:
+    {
+      text: "",
+      nextDialogue: 0,
+      addScore: 0,
+    },
+    24:
+    {
+      text: "",
+      nextDialogue: 0,
+      addScore: 0,
+    },
+  },
+  
 }
 
 
