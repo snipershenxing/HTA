ca7d4f46b4fd2ebf04f89442a43d6016f0287940
Albert Saputra <a.albert.saputra@gmail.com>
2020-03-11 12:52:46 -0700

    [fix] disconnected link of files

--- client/src/components/DonorBubble.jsx
+++ client/src/components/DonorBubble.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const DonorBubble = ({ text, nextArray, dialogueHandler }) => {
+const DonorBubble = ({ text, nextArray, dialogueHandler, donorClickable }) => {
   return (
-    <div onClick={() => dialogueHandler(false, nextArray)} className='respondButton'>
+    <div
+      id='respondButton'
+      className='respondButton'
+      onClick={() => {
+        document.getElementById('respondButton').style.filter = 'none';
+        dialogueHandler(false, nextArray);
+      }}
+      onMouseEnter={() => { if (donorClickable) document.getElementById('respondButton').style.filter = 'brightness(0.7)'; }}
+      onMouseLeave={() => { if (donorClickable) document.getElementById('respondButton').style.filter = 'none'; }}
+    >
       <p className='textForRespondButton'>
         {text}
       </p>
